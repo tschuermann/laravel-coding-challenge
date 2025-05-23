f769d1815ccb54c6b1955275a4e5151555f0304f
tschuermann <contact@tschuermann.de>
2025-05-23 16:14:42 +0200

    Update database script

--- database/migrations/2025_05_21_174510_create_challenges_table.php
+++ database/migrations/2025_05_21_174510_create_challenges_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('challenges', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
             $table->string('image_path')->nullable();
             $table->string('title');
             $table->text('body');
